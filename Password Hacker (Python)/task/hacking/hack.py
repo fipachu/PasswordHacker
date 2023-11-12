95ef88df26dbc668c252023ad1a850ad30d529df
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2023-11-12 21:21:52 +0100

    Solve stage 1/5

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -1,1 +1,26 @@
-# write your code here
+import socket
+import argparse
+
+
+def config() -> tuple[tuple[str, int], bytes]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip')
+    parser.add_argument('port', type=int)
+    parser.add_argument('message')
+    args = parser.parse_args()
+    return (args.ip, args.port), args.message.encode()
+
+
+def main():
+    address, message = config()
+
+    with socket.socket() as client:
+        client.connect(address)
+        client.send(message)
+        response = client.recv(1024).decode()
+
+        print(response)
+
+
+if __name__ == "__main__":
+    main()
