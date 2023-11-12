97eb67ca685f9d78b0dbf1d929348c63e2d2fe38
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2023-11-12 21:27:37 +0100

    Move config() to init

--- Password Hacker (Python)/task/hacking/hack.py
+++ Password Hacker (Python)/task/hacking/hack.py
@@ -8,26 +8,23 @@ class Config:
     address: tuple[str, int]
     message: bytes
 
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('ip')
+        parser.add_argument('port', type=int)
+        parser.add_argument('message')
+        args = parser.parse_args()
 
-def config() -> Config:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('ip')
-    parser.add_argument('port', type=int)
-    parser.add_argument('message')
-    args = parser.parse_args()
-
-    return Config(
-        (args.ip, args.port),
-        args.message.encode()
-    )
+        self.address = args.ip, args.port
+        self.message = args.message.encode()
 
 
 def main():
-    con = config()
+    config = Config()
 
     with socket.socket() as client:
-        client.connect(con.address)
-        client.send(con.message)
+        client.connect(config.address)
+        client.send(config.message)
         response = client.recv(1024).decode()
 
         print(response)
