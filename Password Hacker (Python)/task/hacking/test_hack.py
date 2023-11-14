14298c6394d30f94bfbc3927477c1b9526f6c8ea
Filip Łapkiewicz <80906036+fipachu@users.noreply.github.com>
2023-11-14 01:44:45 +0100

    Refactor names in test_hack.py

--- Password Hacker (Python)/task/hacking/test_hack.py
+++ Password Hacker (Python)/task/hacking/test_hack.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools as it
 from unittest import TestCase
 
 # Comment out next line as a workaround to fix the submission test
@@ -9,14 +9,14 @@ ALPHABET = 'abcdefghijklmnopqrstuvwxyz0123456789'
 ONE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
        'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-TWO = (''.join(chars) for chars in itertools.product(ONE, repeat=2))
-THREE = (''.join(chars) for chars in itertools.product(ONE, repeat=3))
+TWO = (''.join(chars) for chars in it.product(ONE, repeat=2))
+THREE = (''.join(chars) for chars in it.product(ONE, repeat=3))
 
 
 class Test(TestCase):
     def test_get_passwords(self):
-        s = get_passwords()
-        test_s = itertools.chain(ONE, TWO, THREE)
+        actual = get_passwords()
+        expected = it.chain(ONE, TWO, THREE)
 
-        for letter in test_s:
-            self.assertEqual(next(s), letter)
+        for password in expected:
+            self.assertEqual(next(actual), password)
